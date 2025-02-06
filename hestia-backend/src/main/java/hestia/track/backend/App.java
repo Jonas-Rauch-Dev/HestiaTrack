bffe286b97e1197f82fe90d1af70d7cda7db56eb
Jonas <jonasrauch23@gmail.com>
2025-02-06 19:32:18 +0100

    chor: initial commit

---
+++ hestia-backend/src/main/java/hestia/track/backend/App.java
@@ -0,0 +1,10 @@
+package hestia.track.backend;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ hestia-frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hestia Track</h1>
+    </div>
+  );
+}
